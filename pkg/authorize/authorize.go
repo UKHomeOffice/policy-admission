e7877e8200c8b5df2fe2bd6a7b4736843112a331
Rohith <gambol99@gmail.com>
2017-10-25 13:00:10 +0000

    ImageList Authorizer
    - adding the imagelist authorizer plugin

--- pkg/authorize/authorize.go
+++ pkg/authorize/authorize.go
@@ -8,9 +8,6 @@ You may obtain a copy of the License at
     http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
-	c.RLock()
-	defer c.RUnlock()
-	return c.authorizer.Name()
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
@@ -24,6 +21,7 @@ import (
 
 	"github.com/UKHomeOffice/policy-admission/pkg/api"
 	"github.com/UKHomeOffice/policy-admission/pkg/authorize/domains"
+	"github.com/UKHomeOffice/policy-admission/pkg/authorize/imagelist"
 	"github.com/UKHomeOffice/policy-admission/pkg/authorize/images"
 	"github.com/UKHomeOffice/policy-admission/pkg/authorize/namespaces"
 	"github.com/UKHomeOffice/policy-admission/pkg/authorize/securitycontext"
@@ -46,6 +44,8 @@ func newAuthorizer(name, path string) (api.Authorize, error) {
 		return domains.NewFromFile(path)
 	case images.Name:
 		return images.NewFromFile(path)
+	case imagelist.Name:
+		return imagelist.NewFromFile(path)
 	case namespaces.Name:
 		return namespaces.NewFromFile(path)
 	case securitycontext.Name:
---
+++ pkg/authorize/imagelist/doc.go
@@ -0,0 +1,44 @@
+package imagelist
+
+import "time"
+
+const (
+	// Name is the name of the authorizer
+	Name = "imagelist"
+)
+
+// Config is the configuration for the service
+type Config struct {
+	// CacheFailure indicates we shoud cache failures for x duration
+	CacheFailure time.Duration `yaml:"cache-failure" json:"cache-failure"`
+	// CacheSuccess indicates we should cache successful requests for x duration
+	CacheSuccess time.Duration `yaml:"cache-success" json:"cache-success"`
+	// ClientCA is the path for a CA file
+	ClientCA string `yaml:"client-ca" json:"client-ca"`
+	// ClientCertificateCert is the path to a client certificate to use
+	ClientCertificateCert string `yaml:"client-cert" json:"client-cert"`
+	// ClientCertificateKey is the path to a client certificate to use
+	ClientCertificateKey string `yaml:"client-key" json:"client-key"`
+	// EndpointURL is the external endpoint to call
+	EndpointURL string `yaml:"endpoint-url" json:"endpoint-url"`
+	// SkipTLSVerify indicated we skip the TLS for the external endpoint
+	SkipTLSVerify bool `yaml:"skip-tls-verify" json:"skip-tls-verify"`
+	// IgnoredNamespaces is a list namespaces to ignore
+	IgnoreNamespaces []string `yaml:"ignore-namespaces" json:"ignore-namespaces"`
+	// Name is an arbitary name for this webhook
+	Name string `yaml:"name" json:"name"`
+	// Timeout is the duration between timing out
+	Timeout time.Duration `yaml:timeout" json:"timeout"`
+	// Token is a bearer token to use on requests
+	Token string `yaml:"token" json:"token"`
+}
+
+// NewDefaultConfig returns a default config
+func NewDefaultConfig() *Config {
+	return &Config{
+		CacheFailure:     time.Duration(1 * time.Minute),
+		CacheSuccess:     time.Duration(3 * time.Minute),
+		IgnoreNamespaces: []string{"kube-system", "kube-admission", "kube-public"},
+		Timeout:          time.Duration(5 * time.Second),
+	}
+}
